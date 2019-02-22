4d96bf1018e499f865363a7ca8b16cc693334811
Rodrigo Bresan <rcbresan@gmail.com>
2019-02-22 19:09:53 -0300

    Cleanup on EmbeddingNetwork

--- EmbeddingNetwork.py
+++ EmbeddingNetwork.py
@@ -28,20 +28,9 @@ class EmbeddingNetwork(Model):
         return X_list
 
     def __make_model(self):
-        categories = self.config.categories
+        inputs, outputs = self.make_embedding_layers()
 
-        embedding_inputs = []
-        embedding_outputs = []
-        for category in categories:
-            input_category = Input(shape=(1,))
-            output_category = Embedding(category.unique_values, category.embedding_size, name=category.alias)(
-                input_category)
-            output_category = Reshape(target_shape=(category.embedding_size,))(output_category)
-
-            embedding_inputs.append(input_category)
-            embedding_outputs.append(output_category)
-
-        output_model = Concatenate()(embedding_outputs)
+        output_model = Concatenate()(outputs)
         output_model = Dense(1000, kernel_initializer="uniform")(output_model)
         output_model = Activation('relu')(output_model)
         output_model = Dense(500, kernel_initializer="uniform")(output_model)
@@ -49,8 +38,24 @@ class EmbeddingNetwork(Model):
         output_model = Dense(2)(output_model)
         output_model = Activation('sigmoid')(output_model)
 
-        self.model = KerasModel(inputs=embedding_inputs, outputs=output_model)
+        self.model = KerasModel(inputs=inputs, outputs=output_model)
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    def make_embedding_layers(self):
+        embedding_inputs = []
+        embedding_outputs = []
+
+        for category in self.config.categories:
+            input_category = Input(shape=(1,))
+            output_category = Embedding(input_dim=category.unique_values,
+                                        output_dim=category.embedding_size,
+                                        name=category.alias)(input_category)
+            output_category = Reshape(target_shape=(category.embedding_size,))(output_category)
+
+            embedding_inputs.append(input_category)
+            embedding_outputs.append(output_category)
+
+        return embedding_inputs, embedding_outputs
 
     def fit(self, X_train, y_train, X_val, y_val):
         self.model.fit(self.split_features_from_categories(X_train, self.config.categories), y_train,
